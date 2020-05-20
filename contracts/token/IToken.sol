b47bcf17284e07ae9351cda4867537878ad569c9
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-05-20 14:42:46 -0400

    Create IToken.sol
    add abridged token interface (erc-20)

---
+++ contracts/token/IToken.sol
@@ -0,0 +1,5 @@
+interface IToken { // brief ERC-20 interface
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
