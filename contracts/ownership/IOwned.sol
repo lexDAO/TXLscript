98d859201d6f9a01e54343ae1501ecb416524333
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-06-01 15:49:35 -0400

    Create IOwned.sol
    add simple owner scrip from Bancor Ether token: https://etherscan.io/address/0xc0829421c1d260bd3cb3e0f06cfe2d52db2ce315#code

---
+++ contracts/ownership/IOwned.sol
@@ -0,0 +1,7 @@
+contract IOwned {
+    // this function isn't abstract since the compiler emits automatically generated getter functions as external
+    function owner() public constant returns (address owner) { owner; }
+
+    function transferOwnership(address _newOwner) public;
+    function acceptOwnership() public;
+}
